85633cf7af056380ea89f8cacf16587194b93ae7
benj-lazaro <benjie.work@gmail.com>
2023-11-02 01:15:16 +0800

    Chapter 02 simple_messages.py initial commit

---
+++ 02-variables-simple-data-types/02-simple-messages/simple_messages.py
@@ -0,0 +1,8 @@
+# Assign a message to a variable then print it
+# Then replace the content of the variable and then print it
+
+message_text = "This is the initial text."
+print(message_text)
+
+message_text = "This is the replacement text."
+print(message_text)
