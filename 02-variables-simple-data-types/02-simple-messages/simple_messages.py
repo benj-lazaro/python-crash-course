de52692a56ff1906e726c790831d14bdd214fffd
benj-lazaro <benjie.work@gmail.com>
2023-12-12 22:33:41 +0800

    Chapter 02 simple_message.py code refactor

--- 02-variables-simple-data-types/02-simple-messages/simple_messages.py
+++ 02-variables-simple-data-types/02-simple-messages/simple_messages.py
@@ -1,8 +1,7 @@
 # Assign a message to a variable then print it
-# Then replace the content of the variable and then print it
-
 message_text = "This is the initial text."
 print(message_text)
 
+# Then replace the content of the variable and then print it
 message_text = "This is the replacement text."
 print(message_text)
